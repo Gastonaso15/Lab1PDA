506eb171a1983908ddda1b19405d8fa128a81a67
Agustin Machado <agusm.s.2006@gmail.com>
2025-09-10 08:36:30 -0300

    Docker Arreglado

--- src/main/java/culturarte/persistencia/JPAUtil.java
+++ src/main/java/culturarte/persistencia/JPAUtil.java
@@ -5,8 +5,18 @@ import jakarta.persistence.Persistence;
 import jakarta.persistence.EntityManager;
 
 public class JPAUtil {
+
+    private static final String PERSISTENCE_UNIT;
+
+    static {
+        if (System.getenv("DOCKER") != null) {
+            PERSISTENCE_UNIT = "culturartePU-docker";
+        } else {
+            PERSISTENCE_UNIT = "culturartePU";
+        }
+    }
     public static final EntityManagerFactory emf =
-            Persistence.createEntityManagerFactory("culturartePU");
+            Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
 
     public static EntityManager getEntityManager() {
         return emf.createEntityManager();
